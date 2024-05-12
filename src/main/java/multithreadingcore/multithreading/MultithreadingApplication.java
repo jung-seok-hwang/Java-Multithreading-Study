6e210e1fa4ffb5348468e0de9879dfc3fcd3b587
Joong Seok Hwang <hjs7949@naver.com>
2024-05-12 13:59:09 +0900

    🎉init : Init project

---
+++ src/main/java/multithreadingcore/multithreading/MultithreadingApplication.java
@@ -0,0 +1,13 @@
+package multithreadingcore.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/multithreadingcore/multithreading/MultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package multithreadingcore.multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
