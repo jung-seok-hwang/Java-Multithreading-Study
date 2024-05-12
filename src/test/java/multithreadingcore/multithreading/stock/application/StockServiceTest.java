becb362f88b96189c42e656a1dd3960f4323aa17
Joong Seok Hwang <hjs7949@naver.com>
2024-05-12 14:01:23 +0900

    ✨feat: stock api test code

---
+++ src/test/java/multithreadingcore/multithreading/stock/application/StockServiceTest.java
@@ -0,0 +1,83 @@
+package multithreadingcore.multithreading.stock.application;
+
+import lombok.RequiredArgsConstructor;
+import multithreadingcore.multithreading.stock.entity.Stock;
+import multithreadingcore.multithreading.stock.repository.StockRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void multithreadingTest1() throws InterruptedException {
+        int threadCount = 100;
+
+        //thread pool
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        Assertions.assertEquals(0, stock.getQuantity());
+    }
+
+    @Test
+    public void pessimisticLocking() throws InterruptedException {
+        int threadCount = 100;
+        //thread pool
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    int count = 1;
+                    stockService.pessimisticLocking(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        Assertions.assertEquals(0, stock.getQuantity());
+    }
+}
