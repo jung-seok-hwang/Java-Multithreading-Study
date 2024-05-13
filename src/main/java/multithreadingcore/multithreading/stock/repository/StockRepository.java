476677932c77d6901c1cbaa2cfff867267f41844
Joong Seok Hwang <hjs7949@naver.com>
2024-05-13 14:15:07 +0900

    ♻️ refactor: add pessimistic write lock query

--- src/main/java/multithreadingcore/multithreading/stock/repository/StockRepository.java
+++ src/main/java/multithreadingcore/multithreading/stock/repository/StockRepository.java
@@ -1,9 +1,18 @@
 package multithreadingcore.multithreading.stock.repository;
 
 
+import jakarta.persistence.LockModeType;
+
 import multithreadingcore.multithreading.stock.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(@Param("id")Long id);
 
 }
